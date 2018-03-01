7a5afee96a5ce0c8bcab0ba710a2393141711521
ha quang trung <haquangtrungceo@gmail.com>
2018-03-01 15:43:43 +0700

    update main

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@
  */
 
 /**
- * @description:  
+ * @description:
  * @author: Admin
  * @time: 3:36:19 PM
  * @date: Mar 1, 2018
@@ -11,17 +11,17 @@
 public class main {
 
 	/**
-	 * @description:  
+	 * @description:
 	 * @author: Admin CMC Corporation
 	 * @create_date: Mar 1, 2018
 	 * @modifier: Admin
 	 * @modifined_date: Mar 1, 2018
-	 * @exception: 
+	 * @exception:
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("xin chào");
 	}
 
 }
