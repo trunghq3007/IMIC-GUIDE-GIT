9ab96e7af675dcd40157cffca229ea3d66a7b8cb
ha quang trung <haquangtrungceo@gmail.com>
2018-03-01 15:41:07 +0700

    add project

---
+++ src/main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @description:  
+ * @author: Admin
+ * @time: 3:36:19 PM
+ * @date: Mar 1, 2018
+ */
+public class main {
+
+	/**
+	 * @description:  
+	 * @author: Admin CMC Corporation
+	 * @create_date: Mar 1, 2018
+	 * @modifier: Admin
+	 * @modifined_date: Mar 1, 2018
+	 * @exception: 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
