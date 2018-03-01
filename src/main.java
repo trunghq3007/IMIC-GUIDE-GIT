1709229b3b7389e673778889227a4fb1e833e8ed
ha quang trung <haquangtrungceo@gmail.com>
2018-03-01 15:47:00 +0700

    add z.java

--- src/main.java
+++ src/main.java
@@ -22,6 +22,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("xin chào");
+		System.out.println("xin chào git");
+
 	}
 
 }
