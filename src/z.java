07bfc01b4abe790a1d5cb05a20be78d6c8c447e4
hqtrung3007 <hqtrung3007@gmail.com>
2018-03-02 14:05:03 +0700

    hqtrung3007 commit

--- src/z.java
+++ src/z.java
@@ -9,18 +9,4 @@
  * @date: Mar 1, 2018
  */
 public class z {
-// change
-	// change
-	/**
-	* @description:  
-	* @author: Admin CMC Corporation
-	* @create_date: Mar 2, 2018
-	* @modifier: Admin
-	* @modifined_date: Mar 2, 2018
-	* @exception: 
-	*/
-	private void sysout() {
-		// TODO Auto-generated method stub
-
-	}
 }
